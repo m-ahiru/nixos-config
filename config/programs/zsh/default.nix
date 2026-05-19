ad03e9c973ead42402e132f7f7df455abc2e6ed4
m-ahiru <strafearbeit@gmail.com>
2026-05-19 21:37:38 +0200

    sdfsdf

--- config/programs/zsh/default.nix
+++ config/programs/zsh/default.nix
@@ -11,13 +11,6 @@
     initContent = builtins.readFile ./zsh-init.sh;
     shellAliases = {
       rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#mahiru |& nom";
-      ds3 = "protonhax run 374320 /home/mahiru/.wine/drive_c/Program\\ Files/Cheat\\ Engine/Cheat\\ Engine.exe";
-      p5 = "protonhax run 1687950 /home/mahiru/.wine/drive_c/Program\\ Files/Cheat\\ Engine/Cheat\\ Engine.exe";
-      sbr = "protonhax run 3489700 /home/mahiru/.wine/drive_c/Program\\ Files/Cheat\\ Engine/Cheat\\ Engine.exe";
-      er = "protonhax run 1245620 /home/mahiru/.wine/drive_c/Program\\ Files/Cheat\\ Engine/Cheat\\ Engine.exe";
-      ertool = "protonhax run 1245620 /home/mahiru/Documents/EldenRingTool.exe";
-      ds3tool = "protonhax run 374320 /home/mahiru/Documents/SilkySouls3/SilkySouls3.exe";
-      ttool = "protonhax run 1245620 /home/mahiru/Documents/TarnishedTool.exe";
       term = "setsid kitty --class ScratchpadTerm --title Scratchpad >/dev/null 2>&1 &";
       gubtool = "~/.cargo/bin/gubtool";
       compress = "/etc/nixos/config/programs/zsh/Scripts/resize_video.sh";
@@ -27,7 +20,6 @@
       claude = "setsid chromium --app=https://claude.ai --class Claude >/dev/null 2>&1 &";
       whatsapp = "setsid chromium --app=https://web.whatsapp.com --class WhatsApp >/dev/null 2>&1 &";
       soundcloud = "setsid chromium --app=https://soundcloud.com --class Soundcloud >/dev/null 2>&1 &";
-      scratchpad = "setsid foot --class ScratchpadTerm --title Scratchpadkitty &";
     };
 
     oh-my-zsh = {
