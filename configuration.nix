799c0193fb1b56818b302a547d10a9f492581245
m-ahiru <strafearbeit@gmail.com>
2026-05-25 18:36:18 +0200

    sdfdsf

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,10 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
+###################################################
+#### NOTE TO SELF:
+###  PROTON_REMOTE_DEBUG_CMD="/home/mahiru/.wine/drive_c/Program\ Files/Cheat\ Engine/Cheat\ Engine.exe" %command%
+###################################################
 
 { config, pkgs, lib, inputs, ... }:
 
