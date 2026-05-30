5088fff34850972fd1a3a0067685fe7cfe41c9c3
m-ahiru <strafearbeit@gmail.com>
2026-05-30 10:27:56 +0200

    yt-dlp thing

--- config/programs/zsh/default.nix
+++ config/programs/zsh/default.nix
@@ -14,6 +14,7 @@
       term = "setsid kitty --class ScratchpadTerm --title Scratchpad >/dev/null 2>&1 &";
       gubtool = "~/.cargo/bin/gubtool";
       compress = "/etc/nixos/config/programs/zsh/Scripts/resize_video.sh";
+      dlvideo = ''yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]" --merge-output-format mp4 -P ~/Videos'';
       crop43 = "/etc/nixos/config/programs/zsh/Scripts/crop43.sh";
       # ac = "cd ~/Apps/uClicker/;./uClicker";
       upscale = "/etc/nixos/config/programs/zsh/Scripts/upscale.sh";
