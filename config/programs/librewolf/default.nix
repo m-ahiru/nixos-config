f4eceed2fea5f0a56831b5d679d14162601b8722
m-ahiru <strafearbeit@gmail.com>
2026-05-30 13:01:21 +0200

    more librewolf stuff

--- config/programs/librewolf/default.nix
+++ config/programs/librewolf/default.nix
@@ -1,4 +1,18 @@
 { config, ... }:
 {
     home.file.".config/librewolf/librewolf/ltlhhlc4.default/chrome/userChrome.css".source = ./assets/userChrome.css;
+    home.file.".config/librewolf/librewolf/ltlhhlc4.default/user.js".text = ''
+  user_pref("browser.startup.homepage", "search.mahiru.dev");
+  user_pref("browser.newtabpage.enabled", false);
+  user_pref("browser.toolbars.bookmarks.visibility", "never");
+  user_pref("browser.warnOnQuitShortcut", false);
+  user_pref("general.autoScroll", true);
+  user_pref("media.eme.enabled", true);
+  user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+  user_pref("permissions.default.webgl", true);
+  user_pref("privacy.resistFingerprinting", false);
+  user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+  user_pref("webgl.force-enabled", true);
+  user_pref("layout.css.prefers-color-scheme.content-override", 0);
+'';
 }
