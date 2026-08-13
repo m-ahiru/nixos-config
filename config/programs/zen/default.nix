d57590de157461cedff0ff0b84367e643eac35f7
m-ahiru <strafearbeit@gmail.com>
2026-08-13 19:47:41 +0200

    update

--- config/programs/zen/default.nix
+++ config/programs/zen/default.nix
@@ -3,17 +3,25 @@
     home.file.".config/zen/0qaddgqk.Default Profile/chrome/userChrome.css".source = ./assets/zen-userChrome.css;
     home.file.".config/zen/0qaddgqk.Default Profile/chrome/userContent.css".source = ./assets/zen-userContent.css;
     home.file.".config/zen/0qaddgqk.Default Profile/user.js".text = ''
-  user_pref("browser.startup.homepage", "search.mahiru.dev");
-  user_pref("browser.newtabpage.enabled", false);
-  user_pref("browser.toolbars.bookmarks.visibility", "never");
-  user_pref("browser.warnOnQuitShortcut", false);
-  user_pref("general.autoScroll", true);
-  user_pref("media.eme.enabled", true);
-  user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
-  user_pref("permissions.default.webgl", true);
-  user_pref("privacy.resistFingerprinting", false);
+// ---- userChrome/userContent laden ----
   user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-  user_pref("webgl.force-enabled", true);
-  user_pref("layout.css.prefers-color-scheme.content-override", 0);
+
+  // ---- Transparenz ----
+  user_pref("widget.transparent-windows", true);
+  user_pref("browser.tabs.allow_transparent_browser", true);
+  user_pref("zen.theme.acrylic-elements", true);
+
+  // ---- Start auf search.mahiru.dev ----
+  user_pref("browser.startup.page", 1);
+  user_pref("browser.startup.homepage", "https://search.mahiru.dev");
+
+  // ---- neuer Tab lädt Seite statt floating urlbar ----
+  user_pref("zen.urlbar.replace-newtab", false);
+
+  // ---- keine Tab-Wiederherstellung nach Reboot/Crash ----
+  user_pref("browser.sessionstore.resume_from_crash", false);
+
+  // ---- Inspector auf chrome (optional) ----
+  user_pref("devtools.chrome.enabled", true);
 '';
 }
